d1cda236d24d618fef7030a4be65f10baca1e22a
s-8506 <s.8506.a@gmail.com>
2020-07-29 21:37:24 +0530

    clean

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -65,14 +65,4 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := qcrilhook
-LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/framework/qcrilhook.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
 endif
